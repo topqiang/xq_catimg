f30df6b0ad8cd872f1ba4444c2ffee76cf400daa
topqiang <topqiang@kanziqiang.top>
2016-08-19 14:28:44 +0800

    finished this plugins

--- js/xq_catimg.js
+++ js/xq_catimg.js
@@ -9,20 +9,22 @@
 	var startY;
 	var left;
 	var top;
+	var oldwidth;
+	var oldheight;
 	$.fn.xq_catimg=function(options){
 		var self=$(this);
 		var src=self.attr("src");
-		var width=self.width();
-		var height=self.height();
+		var width=oldwidth=self.width();
+		var height=oldheight=self.height();
 		var imgtop=self.offset().top;
 		var imgleft=self.offset().left;
 		var defaults={
-			"imgwidth":400,//设置图片最大宽度  默认200px
-			"imgheight":600,//设置图片最大高度  默认200px
+			"imgwidth":500,//设置图片最大宽度  默认200px
+			"imgheight":400,//设置图片最大高度  默认200px
 			"showview":true,//设置图片预览区域
 		}
 		$.extend(defaults, options);
-		this,init();
+		init();
 		function init(){
 			self.parent().append('<div class="imgbag" style="width:'+defaults.imgwidth+'px;height:'+defaults.imgheight+'px"></div><div class="viewimg"></div>');
 			self.appendTo("div.imgbag");
@@ -38,8 +40,8 @@
 				height=self.height();
 				width=self.width();
 			}
-	//		imgtop=self.offset().top;
-	//		imgleft=self.offset().left;
+//			imgtop=self.offset().top;
+//			imgleft=self.offset().left;
 			chaX=self.offset().left-self.parent().offset().left;//获取图片自定义后和边框之间的距离
 			chaY=self.offset().top-self.parent().offset().top;//同上
 			putmove(self.nextAll(".viewport"));//加入移动事件
@@ -58,12 +60,16 @@
 			startY=e.clientY;
 			left=startX-imgleft;
 			top=startY-imgtop;
+			$('.imgbag .viewport').css({"display":"none"});
+			$('.imgbag .clipimg').css({"display":"none"});
+			$(".viewimg").css({"top":(imgtop+10)+"px","left":(imgleft+defaults.imgwidth+20)+"px","position":"fixed","display":"none","z-index":"999"});
 			$(".viewimg").html('<img src="'+src+'" class="showview" ondragstart="return false"/>');
-			$(".viewimg").css({"top":(imgtop+10)+"px","left":(imgleft+width+20)+"px","position":"fixed"});
 			$('.imgbag').on('mousemove',function(e){
 				if (keytype) {
 					return;
 				}
+				var flag=e.clientX>(imgleft+chaX) && e.clientY>(imgtop+chaY) && e.clientX<(imgleft+chaX+width) && e.clientY<(imgtop+chaY+height);
+				if(!flag)return;
 				var nwidth=e.clientX-startX;
 				var nheight=e.clientY-startY;
 				if(nwidth<0){
@@ -74,6 +80,8 @@
 				}
 				vwidth=Math.abs(nwidth);
 				vheight=Math.abs(nheight);
+				$('.imgbag .viewport').css({"display":"block"});
+				$('.imgbag .clipimg').css({"display":"block"});
 				$('.imgbag .viewport').css({"top":top+"px","left":left+"px","width":vwidth+"px","height":vheight+"px"});
 				$('.imgbag .clipimg').css({"clip":"rect("+(top-chaY)+"px,"+(left+vwidth-chaX)+"px,"+(top+vheight-chaY)+"px,"+(left-chaX)+")"});
 				showView();
@@ -91,24 +99,50 @@
 					if (keytype) {
 						return;
 					}
-					console.log("移动");
 					var czX=e.clientX-startX+curX;
 					var czY=e.clientY-startY+curY;
-					top=czY;
-					left=czX;
-					$viewport.css({"left":czX+"px","top":czY+"px"});
-					$viewport.prevAll(".clipimg").css({"clip":"rect("+(czY-chaY)+"px,"+(czX+vwidth-chaX)+"px,"+(czY+vheight-chaY)+"px,"+(czX-chaX)+"px)"});
+					var flag=true;
+					if ((czX-chaX)>0 && (czX+vwidth-chaX)<width) {
+						$viewport.css({"left":czX+"px"});
+						left=czX;
+						$viewport.prevAll(".clipimg").css({"clip":"rect("+(top-chaY)+"px,"+(left+vwidth-chaX)+"px,"+(top+vheight-chaY)+"px,"+(left-chaX)+"px)"});
+					}
+					if ((czY-chaY)>0 && (czY+vheight-chaY)<height) {
+						$viewport.css({"top":czY+"px"});
+						top=czY;
+						$viewport.prevAll(".clipimg").css({"clip":"rect("+(top-chaY)+"px,"+(left+vwidth-chaX)+"px,"+(top+vheight-chaY)+"px,"+(left-chaX)+"px)"});
+					}
+					showView();
 				});
 			});
 		}
 		function showView(){
-			
-			$(".showview").css({"position":"absolute","clip":"rect("+(top-chaY)+"px,"+(left+vwidth-chaX)+"px,"+(top+vheight-chaY)+"px,"+(left-chaX)+")"});
+			var showtop=top-chaY;
+			var showleft=left-chaX;
+			$(".viewimg").css({"display":"block"});
+			$(".showview").css({"top":-showtop+"px","left":-showleft+"px","width":width+"px","height":height+"px","position":"absolute","clip":"rect("+(top-chaY)+"px,"+(left+vwidth-chaX)+"px,"+(top+vheight-chaY)+"px,"+(left-chaX)+")"});
+		}
+		function getCatData(){
+			var bili=oldwidth/width;
+			var showtop=top-chaY;
+			var showleft=left-chaX;
+			var newleft=showleft*bili;
+			var newtop=showtop*bili;
+			var newheight=vheight*bili;
+			var newwidth=vwidth*bili;
+			return {"top":newtop,"left":newleft,"width":newwidth,"height":newheight};
+		}
+		window['xq_catimg']={
+			getCatData:function(){
+				return getCatData();
+			}
 		}
 		$(window).on("mousemove",function(e){
 			if(!ifkeydown || !keytype){
 				return;
 			}
+			var flag=e.clientX>(imgleft+chaX) && e.clientY>(imgtop+chaY) && e.clientX<(imgleft+chaX+width) && e.clientY<(imgtop+chaY+height);
+			if(!flag)return;
 			var nwidth=e.clientX-startX;
 			var nheight=e.clientY-startY;
 			if (keytype == "top") {
@@ -166,6 +200,7 @@
 				startY=e.clientY;
 				startX=e.clientX;
 			}
+			showView();
 			e.preventDefault();
 			e.stopPropagation();
 		});
@@ -179,4 +214,3 @@
 		});
 	}
 })(jQuery);
-$(".niceimg").xq_catimg();
